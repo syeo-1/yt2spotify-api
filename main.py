e1bf76fcee6a46211e970ce6746856d44f80173b
syeo-1 <seansqyeo@gmail.com>
2024-11-05 11:40:53 -0800

    requirements and start api keys file

--- main.py
+++ main.py
@@ -8,5 +8,14 @@ YOUTUBE_PLAYLIST_URL = "https://www.youtube.com/playlist"
 
 @app.get("/youtube/{_:path}")
 def retrieve_playlist_json(request: Request, q: Union[str, None] = None):
+
+    # using the playlist id, retrieve names of all the songs
+    
+    # then, check using the spotify api if these songs actually exist
+
+    # if the song doesn't exist, first check if for the link of each song
+        # if it's a self contained playlist, then use a regex to try and
+        # retrieve song names for those songs within the compilation
+
     return {"path": YOUTUBE_PLAYLIST_URL,
             "playlist_id": request.url.query}
